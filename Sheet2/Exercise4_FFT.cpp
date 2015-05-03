e542084ef744ccc684ec889cf00751aee9fcfdf8
FG_TUM <reaper.tours@googlemail.com>
2015-05-03 17:45:58 +0200

    lalala

--- Sheet2/Exercise4_FFT.cpp
+++ Sheet2/Exercise4_FFT.cpp
@@ -1,7 +1,16 @@
 /*
- * Exercise4.cpp
+ * Exercise4_FFT.cpp
  *
  *  Created on: 3 May 2015
  *      Author: seriously
  */
 
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+//	degree of polynomials
+	int degree = 2;
+
+	return EXIT_SUCCESS;
+}
