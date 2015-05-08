debf4c037aacddd493fbfe29d38db7e72c166d38
FG-TUM <f.gratl@tum.de>
2015-05-09 01:20:33 +0200

    restructured

--- Sheet3/Exercise4_FermatTest.hs
+++ Sheet3/Exercise4_FermatTest.hs
@@ -4,9 +4,9 @@ import Data.Bits
 -- fermat test with random a
 fermatTestRand :: Integer -> StdGen -> Bool
 fermatTestRand n gen
-  | even n               = False
-  | mod (a^(n-1)) n /= 1 = False
-  | otherwise            = True
+  | even n                      = False
+  | (fastModExp a (n-1) n) /= 1 = False
+  | otherwise                   = True
   where (a, _) = randomR (2, n-1) gen
 
 -- fermat test with given a
@@ -24,6 +24,21 @@ fermatTest2to1000 n = and $ map (\x -> fermatTestA n x) [2..1000]
 fermatTest2toN :: Integer -> Bool
 fermatTest2toN n = and $ map (\x -> fermatTestA n x) [2..(n-1)]
 
+-- fast version of y = a^n mod x
+fastModExp :: Integer -> Integer -> Integer -> Integer
+fastModExp a n x = fastModExpAux (mod a x) n x 1
+
+fastModExpAux :: Integer -> Integer -> Integer -> Integer -> Integer
+fastModExpAux b n x y
+  | n == 0        = y
+  | n .&. 1 == 1  = fastModExpAux (mod (b*b) x) (shiftR n 1) x (mod (y*b) x)
+  | otherwise     = fastModExpAux (mod (b*b) x) (shiftR n 1) x y
+
+main :: IO()
+--main = print $ fermatTest2toN 306450443005841
+main = print $ fermatTest2to1000 306450443005841
+
+-- UNUSED
 -- fast version for y = a^n
 fastExp :: Integer -> Integer -> Integer
 fastExp 0 _ = 0
@@ -46,15 +61,3 @@ modExpAux a n x y
   | n == 0    = y
   | otherwise = modExpAux a (n-1) x (mod (a*y) x)
 
--- fast version of y = a^n mod x
-fastModExp :: Integer -> Integer -> Integer -> Integer
-fastModExp a n x = fastModExpAux (mod a x) n x 1
-
-fastModExpAux :: Integer -> Integer -> Integer -> Integer -> Integer
-fastModExpAux b n x y
-  | n == 0        = y
-  | n .&. 1 == 1  = fastModExpAux (mod (b*b) x) (shiftR n 1) x (mod (y*b) x)
-  | otherwise     = fastModExpAux (mod (b*b) x) (shiftR n 1) x y
-
---main = print $ fermatTestA 306450443005841 1000
-main = print $ modExp 1000 306450443005840 306450443005841
