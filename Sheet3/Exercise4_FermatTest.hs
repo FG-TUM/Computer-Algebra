193bfcaa934aaf31974e1490c91d5834c47f7e99
FG_TUM <reaper.tours@googlemail.com>
2015-05-09 15:53:54 +0200

    added Observations

--- Sheet3/Exercise4_FermatTest.hs
+++ Sheet3/Exercise4_FermatTest.hs
@@ -1,13 +1,5 @@
-import System.Random
+--import System.Random
 import Data.Bits
-
--- fermat test with random a
-fermatTestRand :: Integer -> StdGen -> Bool
-fermatTestRand n gen
-  | even n                      = False
-  | (fastModExp a (n-1) n) /= 1 = False
-  | otherwise                   = True
-  where (a, _) = randomR (2, n-1) gen
 
 -- fermat test with given a
 fermatTestA :: Integer -> Integer -> Bool
@@ -38,7 +30,34 @@ main :: IO()
 --main = print $ fermatTest2toN 306450443005841
 main = print $ fermatTest2to1000 306450443005841
 
--- UNUSED
+
+-- Observations:
+-- The naive implementation of the Fermat test is pretty slow for large n
+-- due to the big exonentiation. Even using the fast exponentiation algorithm
+-- from the lecture (fastExp) doesn't solve the problem as the numbers get
+-- to big (>6KB).
+-- The problem can be solved by a small trick. As we are not interested in the
+-- result of the exponentiation but in in the modulo we can directly calculate
+-- the modulo of the exponentiation. This is possible by exploiting the "rule"
+-- a*b mod c = ((a mod c) * (b mod c)) mod c
+-- This combined with the fastExp algorithm yields a pretty fast modulo
+-- exponentiation (fastModExp) that solves the given problem even for a={2..(n-1)}
+-- in "not measurable short" time. The laziness of Haskell also seems to speed up
+-- the function fermatTest2toN as it might not do further computations as soon
+-- as it gets one "False" which happens first at a = 11633. This tells us that
+-- 306450443005841 is not a prime number, but the Fermat test for a={2..1000}
+-- will not come to this conclusion. Further analysis shows that it is
+-- a Carmichael number.
+
+
+
+-- everything from here on is UNUSED. It only illustrates the evolution of our code.
+
+-- fermat test with random a; for this function import System.Random
+--fermatTestRand :: Integer -> StdGen -> Bool
+--fermatTestRand n gen = fermatTestA n a
+--  where (a, _) = randomR (2, n-1) gen
+
 -- fast version for y = a^n
 fastExp :: Integer -> Integer -> Integer
 fastExp 0 _ = 0
