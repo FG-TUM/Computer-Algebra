17c6524e4a386f6dc7dc356dadfdf80061963de6
FG-TUM <f.gratl@tum.de>
2015-06-04 13:23:20 +0200

    added main and comments

--- Sheet5/Exercise3b_ChineseRemainderTheorem.hs
+++ Sheet5/Exercise3b_ChineseRemainderTheorem.hs
@@ -1,5 +1,9 @@
 module ChineseRemainderTheorem where
 
+main :: IO()
+main = print $ crt [(865, 913), (58, 589), (1168,1403)]
+
+-- Computes CRT for x = y mod n, takes list: [(y_i, n_i)]
 crt :: [(Integer,Integer)] -> Integer
 crt [] = 0
 crt list = crtH list (mulM list)
@@ -8,16 +12,18 @@ crtH :: [(Integer,Integer)] -> Integer -> Integer
 crtH [] _ = 0
 crtH ((a,b) : xs) m = mod (((inverse (div m b) b) * (div m b) * a) + (crtH xs m)) m
 
+-- Computes product of all n_i
 mulM :: [(Integer,Integer)] -> Integer
 mulM [] = 1
 mulM ((a,b):xs) = b * mulM xs
 
+-- Computes gcd by Extended Euclidean Algorithm
 eea :: Integer -> Integer -> (Integer, Integer, Integer)
-eea a b = help a b 1 0 0 1
+eea a b = eeaH a b 1 0 0 1
 
-help :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> (Integer, Integer, Integer)
-help r0 0 s0 _ t0 _ = (r0, s0, t0)
-help r0 r1 s0 s1 t0 t1 = help r1 (r0 - r1*q) s1 (s0 - q*s1) t1 (t0- q*t1)
+eeaH :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> (Integer, Integer, Integer)
+eeaH r0 0 s0 _ t0 _ = (r0, s0, t0)
+eeaH r0 r1 s0 s1 t0 t1 = eeaH r1 (r0 - r1*q) s1 (s0 - q*s1) t1 (t0- q*t1)
   where q = div r0 r1
 
 --inverse of a in b
