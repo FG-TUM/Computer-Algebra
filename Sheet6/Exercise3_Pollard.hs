100985af9bba330871825891d3ae01dfdae8f22e
FG-TUM <f.gratl@tum.de>
2015-06-11 13:22:33 +0200

    Solution for exercise 3

---
+++ Sheet6/Exercise3_Pollard.hs
@@ -0,0 +1,32 @@
+import System.Random
+
+-- determines a propper divisor a of n using a random value x in {0..(n-1)}
+-- randomized version
+pollard :: Integer -> StdGen -> Maybe Integer
+pollard n gen = pollardX n (fst $ randomR (0, (n-1)) gen)
+
+-- version with given x
+pollardX :: Integer -> Integer -> Maybe Integer
+pollardX n x
+  | x >= n = Nothing  -- FAIL
+  | otherwise = pollardAux n (mod (x^2 +1) n) ((x^2 +1)^2 +1)
+
+pollardAux :: Integer -> Integer -> Integer -> Maybe Integer
+pollardAux n x y
+  | 1 < d && d < n  = Just d
+  | d == n          = Nothing --FAIL no conclusion. Maybe run again with different x
+  | otherwise       = pollardAux n (mod (x^2 +1) n) ((y^2 +1)^2 +1)
+  where d = gcd n (x-y)
+
+-- task: factorize 23802996783967
+main :: IO()
+main = do
+  -- generator for random numbers
+  g <- getStdGen
+  print $ pollard 23802996783967 g
+
+-- Outputs vary between 12347 and memory overflow.
+-- As 23802996783967 `div` 12347 = 1927836461 wich is prime 
+-- (checked with aks) all prime-factors are found
+--
+-- solution 23802996783967 = 12347 * 1927836461
