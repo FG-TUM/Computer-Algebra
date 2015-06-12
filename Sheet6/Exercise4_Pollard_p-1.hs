48dc35f8f53af1662780d6ebf49d986d18c22d67
FG-TUM <f.gratl@tum.de>
2015-06-12 14:56:38 +0200

    added sieve of eratosthenes

---
+++ Sheet6/Exercise4_Pollard_p-1.hs
@@ -0,0 +1,24 @@
+import System.Random
+
+-- takes a n to factorize a smoothnes bound b and a random a
+pollardP1 :: Integer -> Integer -> Integer -> Integer
+pollardP1 n b a = undefined
+
+---------------------------------------------------------------------------------------------------
+-----------------------------------------Helper functions------------------------------------------
+---------------------------------------------------------------------------------------------------
+
+-- returns all primes smaller n
+sieveOfEratosthenes :: Integer -> [Integer]
+sieveOfEratosthenes n = takeWhile (<n) unboundSieve
+
+unboundSieve :: [Integer]
+unboundSieve = 2 : sieve [3..] 4 unboundSieve
+  where sieve (x:xs) q (p:t)
+          | x < q     = x : sieve xs q (p:t)
+          | otherwise = sieve (minus xs [q, q+p..]) (head t^2) t
+          where minus a@(x:xs) b@(y:ys) = case compare x y of
+                  LT -> x : minus xs b
+                  EQ ->     minus xs ys
+                  GT ->     minus a  ys
+                minus a b = a
