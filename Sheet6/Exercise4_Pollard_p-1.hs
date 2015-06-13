a08958b92595238aeebeef662f9f58d0e820068f
FG-TUM <f.gratl@tum.de>
2015-06-13 14:36:59 +0200

    WIP

--- Sheet6/Exercise4_Pollard_p-1.hs
+++ Sheet6/Exercise4_Pollard_p-1.hs
@@ -3,14 +3,15 @@ import System.Random
 -- takes a n to factorize a smoothnes bound b and a random a
 pollardP1 :: Integer -> Integer -> Integer -> Integer
 pollardP1 n b a = undefined
+  where primes = sieveOfEratosthenes b
 
----------------------------------------------------------------------------------------------------
------------------------------------------Helper functions------------------------------------------
----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+---------------------------------------Helper functions---------------------------------------
+----------------------------------------------------------------------------------------------
 
 -- returns all primes smaller n
 sieveOfEratosthenes :: Integer -> [Integer]
-sieveOfEratosthenes n = takeWhile (<n) unboundSieve
+sieveOfEratosthenes n = takeWhile (<= n) unboundSieve
 
 unboundSieve :: [Integer]
 unboundSieve = 2 : sieve [3..] 4 unboundSieve
