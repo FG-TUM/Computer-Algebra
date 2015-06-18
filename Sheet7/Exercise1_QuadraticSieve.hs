2dab602c07019323b4463855dd218b5c937ef199
LG-TUM <gartner.ludwig@gmail.com>
2015-06-18 16:06:23 +0200

    test

--- Sheet7/Exercise1_QuadraticSieve.hs
+++ Sheet7/Exercise1_QuadraticSieve.hs
@@ -1,1 +1,21 @@
-jojojo
+--spoda
+tfpp :: Integer -> Maybe (Integer, Integer)
+tfpp n = 
+
+
+
+
+
+
+perfectPowerTest :: Integer -> Maybe (Integer,Integer)
+perfectPowerTest n = perfectPowerTestAux n 2 (floor $ logBase 2 (fromInteger n)) 2 n
+
+perfectPowerTestAux :: Integer -> Integer -> Integer -> Integer -> Integer -> Maybe (Integer,Integer)
+perfectPowerTestAux n e maxE m1 m2
+  | e > maxE    = Nothing
+  | m1 > m2     = perfectPowerTestAux n (e+1) maxE 2 n
+  | mToE == n   = Just (m,e)
+  | mToE >  n   = perfectPowerTestAux n e maxE m1 (m-1)
+  | otherwise   = perfectPowerTestAux n e maxE (m+1) m2
+  where m = div (m1 + m2) 2 -- div truncates towards neg inf
+        mToE = fastExp m e
